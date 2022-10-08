16d0776086f1f5ffaaaec032b49c3c36a2dc700c
Shrey1907 <63794815+Shrey1907@users.noreply.github.com>
2022-10-08 07:16:25 +0530

    Update yes.js

--- yes.js
+++ yes.js
@@ -2,7 +2,7 @@ var clicks=0;
 
 document.querySelector("body").addEventListener("click",function(){
   if (clicks===0){
-    var audio = new Audio("Tum Se Hi (Jab We Met) - Mohit Chauhan - 320Kbps.mp3");
+    var audio = new Audio("BIN TERE SANAM (Remix)-(MafiaRemix.Com).mp3");
     audio.play();
     clicks=clicks+1;
 }
